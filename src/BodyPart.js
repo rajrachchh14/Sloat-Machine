79109450501b58e481b45cb658d2f22d0f036574
rajrachchh14 <rajrachchh14@gmail.com>
2021-09-14 22:03:13 +0530

    1 file modified

--- src/BodyPart.js
+++ src/BodyPart.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 export default function BodyPart() {
+  // console.log(obj1, obj2, obj3);
   let x1 = '🔥';
   let x2 = '🔥';
   let x3 = '🔥';
@@ -13,12 +14,16 @@ export default function BodyPart() {
   let z2 = '😃';
   let z3 = '😃';
 
-  let obj1 = { x1, x2, x3 };
-  let obj2 = { y1, y2, y3 };
-  let obj3 = { z1, z2, z3 };
+  let obj = { x1, x2, x3, y1, y2, y3, z1, z2, z3 };
 
   function PullFunction() {
-    console.log(obj1 ,obj2 , obj3);
+    if (obj.x1 === obj.x2 && obj.x2 === obj.x3) {
+      console.log('x');
+    } else if (obj.y1 === obj.y2 && obj.y2 === obj.y3) {
+      console.log('y');
+    } else if (obj.z1 === obj.z2 && obj.z2 === obj.z3) {
+      console.log('z');
+    }
   }
 
   return (
