46bcd72256138e7d71513bd2f2019283aba4c14f
rajrachchh14 <rajrachchh14@gmail.com>
2021-09-15 00:11:42 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Topbar from './Topbar';
 import BodyPart from './BodyPart';
 
+// import Webcam from 'react-webcam';
 export default function App() {
   return (
     <>
       <div>
         <Topbar />
         <BodyPart />
+        {/* <Webcam /> */}
       </div>
     </>
   );
